0ad7c0323240a57606c9bd4f29ee931f92043c0a
Penkace <penkaceunique@163.com>
2023-05-17 13:36:06 +0800

    Update layers.py

--- CeleTrip/layers.py
+++ CeleTrip/layers.py
@@ -2,12 +2,17 @@ import torch
 import torch.nn.functional as F
 import dgl
 from dgl.nn import GraphConv, AvgPooling, MaxPooling
-####################################################
 import torch.nn as nn
 import torch
 import logging
 from scipy.stats import t
 import math
+
+#########################################
+'''
+Reference : https://github.com/dmlc/dgl/tree/master/examples/pytorch/sagpool
+'''
+#########################################
 
 def get_batch_id(num_nodes:torch.Tensor):
     """Convert the num_nodes array obtained from batch graph to batch_id array
@@ -56,7 +61,6 @@ def topk(x:torch.Tensor, ratio:float, batch_id:torch.Tensor, num_nodes:torch.Ten
     _, perm = dense_x.sort(dim=-1, descending=True)
     perm = perm + cum_num_nodes.view(-1, 1)
     perm = perm.view(-1)
-    # 此处的k是一个向量，每一维是每个子图保留的节点数。k可以设置一个下限，比如为滑动窗口的大小
     k = (ratio * num_nodes.to(torch.float)).ceil().to(torch.long)
     mask = [
         torch.arange(k[i], dtype=torch.long, device=x.device) + 
@@ -88,19 +92,10 @@ class SAGPool(torch.nn.Module):
         self.non_linearity = non_linearity
     
     def forward(self, graph:dgl.DGLGraph, feature:torch.Tensor):
-        # compute socres
         score = self.score_layer(graph, feature).squeeze()
-        # get idx, Z_mask = Z_idx
         perm, next_batch_num_nodes = topk(score, self.ratio, get_batch_id(graph.batch_num_nodes()), graph.batch_num_nodes())
-        # X_out = X'*Z_mask (X'=X_idx)
         feature = feature[perm] * self.non_linearity(score[perm]).view(-1, 1)
         graph = dgl.node_subgraph(graph, perm)
-
-        # node_subgraph currently does not support batch-graph,
-        # the 'batch_num_nodes' of the result subgraph is None.
-        # So we manually set the 'batch_num_nodes' here.
-        # Since global pooling has nothing to do with 'batch_num_edges',
-        # we can leave it to be None or unchanged.
         graph.set_batch_num_nodes(next_batch_num_nodes)
         
         return graph, feature, perm
@@ -144,39 +139,30 @@ class OrientedPool(torch.nn.Module):
         self.ratio = ratio
         self.score_layer = conv_op(in_dim, 1)
         self.non_linearity = non_linearity
-        # 相似度度量
         self.cos_score = nn.CosineSimilarity(dim=1, eps=1e-6)
         self.att_linear = nn.Linear(2, 1, bias=True)
     
     def forward(self, graph:dgl.DGLGraph, feature:torch.Tensor, need_guide = False, special_nodes_idx = None):
         score = self.score_layer(graph, feature).squeeze()
-        
-        # 可以在这里增加与对应节点相似度的计算
-        if need_guide: # 需要用关键信息进行引导
-            # unbatch
-            # sim score
-            # 计算每个图上每个节点对应的相似度得分，然后存在每个图的 sim_score 中，按图进行更新
+        if need_guide: 
+
             graph.ndata["sim_score"] = torch.ones_like(score)
             graph.ndata["feat"] = feature
             graph_list = dgl.unbatch(graph)
-            # for each_graph, person_idx, loc_idx in zip(graph_list, special_node_index):
-            #     # update graph feature
-            #     pass
+
             for each_graph, nodes_idx in zip(graph_list, special_nodes_idx):
                  for node_idx in nodes_idx:
-                    if node_idx == -1: # 不存在special node，则不更新相似度得分
+                    if node_idx == -1: 
                          continue
-                     # get special feature
+                    
                     node_feature = each_graph.ndata["feat"][node_idx]
-                     # repeat it to martix
+                    
                     node_feature = node_feature.repeat(each_graph.num_nodes(), 1)
                     cos_s = self.cos_score(node_feature, each_graph.ndata["feat"])
-                     # update score [累乘相似度]
+                 
                     each_graph.ndata["sim_score"] = each_graph.ndata["sim_score"] * cos_s
-            # batch
             graph = dgl.batch(graph_list)
             sim_score = graph.ndata["sim_score"]
-            # additive attention
             score = torch.reshape(score, (score.shape[0], 1))
             sim_score = torch.reshape(sim_score, (sim_score.shape[0], 1))
             con_score = torch.cat([score, sim_score], 1)
@@ -185,12 +171,6 @@ class OrientedPool(torch.nn.Module):
         perm, next_batch_num_nodes = topk(score, self.ratio, get_batch_id(graph.batch_num_nodes()), graph.batch_num_nodes())
         feature = feature[perm] * self.non_linearity(score[perm]).view(-1, 1)
         graph = dgl.node_subgraph(graph, perm)
-
-        # node_subgraph currently does not support batch-graph,
-        # the 'batch_num_nodes' of the result subgraph is None.
-        # So we manually set the 'batch_num_nodes' here.
-        # Since global pooling has nothing to do with 'batch_num_edges',
-        # we can leave it to be None or unchanged.
         graph.set_batch_num_nodes(next_batch_num_nodes)
         
         return graph, feature, perm
